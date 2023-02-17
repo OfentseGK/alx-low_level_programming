71f1944496b058f92cdf081dbcee6510cc02dc06
Ofentse <ofentse.kganticoe@gmail.com>
2023-02-17 14:53:19 -0800

    alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, then a new line, except q & e
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
